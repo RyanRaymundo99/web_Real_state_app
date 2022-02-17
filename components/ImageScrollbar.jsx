d2f06ff1de1968f09d179c64381ff6295205440e
Ryan Raymundo <90113061+RyanRaymundo99@users.noreply.github.com>
2022-02-17 14:53:00 -0800

    enter

--- components/ImageScrollbar.jsx
+++ components/ImageScrollbar.jsx
@@ -39,7 +39,7 @@ export default function ImageSrollbar({ data }) {
   return (
     <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow} style={{ overflow: 'hidden' }} >
       {data.map((item) => (
-        <Box width='910px' key={item.id} overflow='hidden' p='1'>
+        <Box width='910px' key={item.id} itemID={item.id} overflow='hidden' p='1'>
           <Image placeholder="blur" blurDataURL={item.url} src={item.url} alt="placeholder" width={1000} height={500}  sizes="(max-width: 500px) 100px, (max-width: 1023px) 400px, 1000px"  />
         </Box>
       ))}
